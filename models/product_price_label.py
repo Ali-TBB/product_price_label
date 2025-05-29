e48c2da15eda9eb0e4eda40672f4e18c3d8cab7b
ali <adebbach@gmail.com>
2025-05-29 09:59:05 +0100

    Implement dynamic price label report with configurable dimensions and styles

---
+++ models/product_price_label.py
@@ -0,0 +1,66 @@
+from odoo import api, models
+
+class ReportPriceLabel(models.AbstractModel):
+    _name = 'report.product_price_label.product_price_label_template'
+    _description = 'Dynamic Price Label Report'
+
+
+    def _get_report_values(self, docids, data=None):
+        
+        products = self.env['product.template'].browse(docids)
+        config = self.env['ir.config_parameter'].sudo()
+
+        width = float(config.get_param('product_price_label.paper_width', default='43'))
+        height = float(config.get_param('product_price_label.paper_height', default='35'))
+
+        paperformat_name = f'Dynamic Label {width}x{height} mm'
+        paperformat = self.env['report.paperformat'].search([('name', '=', paperformat_name)], limit=1)
+
+        if not paperformat:
+            paperformat = self.env['report.paperformat'].create({
+                'name': paperformat_name,
+                'format': 'custom',
+                'page_width': width,
+                'page_height': height,
+                'orientation': 'Portrait',
+                'margin_top': 0,
+                'margin_bottom': 0,
+                'margin_left': 0,
+                'margin_right': 0,
+                'header_line': False,
+                'dpi': 90,
+            })
+
+        # Update report to use this paper format
+        report_action = self.env.ref('product_price_label.action_product_price_label')
+        report_action.paperformat_id = paperformat.id
+
+        x = (height * 13) / 35
+        y = (height * 13) / 43
+        font_name = round((x + y) / 2)
+
+
+        x = (height * 12) / 35
+        y = (height * 12) / 43
+        font_barcode = round((x + y) / 2)
+
+
+        x = (height * 18) / 35
+        y = (height * 18) / 43
+        font_price = round((x + y) / 2)
+
+
+        barcode_img_width = round((width * 150) / 43)
+        barcode_img_height = round((height * 50) / 35)
+
+        return {
+            'docs': products,
+            'label_width': width,
+            'label_height': height,
+            'font_name': font_name,
+            'font_barcode': font_barcode,
+            'font_price': font_price,
+            'barcode_img_width': barcode_img_width,
+            'barcode_img_height': barcode_img_height,
+        }
+
